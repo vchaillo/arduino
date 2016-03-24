5b4dcd644fe9ff3973e8cd415deb8934640be7da
vchaillo <valentinchaillou89@gmail.com>
2016-03-24 05:31:22 +0100

    update nunchuck_relais

--- nunchuck_relais/nunchuck_relais.ino
+++ nunchuck_relais/nunchuck_relais.ino
@@ -1,62 +1,67 @@
-/*
- * ArduinoNunchukDemo.ino
- *
- * Copyright 2011-2013 Gabriel Bianconi, http://www.gabrielbianconi.com/
- *
- * Project URL: http://www.gabrielbianconi.com/projects/arduinonunchuk/
- *
- */
-
+#include <Arduino.h>
 #include <Wire.h>
 #include <ArduinoNunchuk.h>
 
 #define BAUDRATE 9600
 
 ArduinoNunchuk nunchuk = ArduinoNunchuk();
-int led = 4;
-int relais = 2;
-int buz = 7;
+const int led = 2;
+const int relais = 4;
+
+int state = LOW;
+bool push_z = true;
+bool push_c = true;
 
 void setup()
 {
   Serial.begin(BAUDRATE);
   pinMode(led, OUTPUT);
   pinMode(relais, OUTPUT);
-  nunchuk.init();   //Initialiser la Nunchuck
+  nunchuk.init();   // Initialiser le nunchuck
 }
 
 void loop()
 {
-  nunchuk.update();  //lecture des donnees
-  //Joystick XY
-  Serial.print(nunchuk.analogX, DEC);  //0-255 X, 125 au neutre
+  nunchuk.update();  // lecture des donnees
+  
+  if (nunchuk.zButton == 1)
+  {
+    if (!digitalRead(led) && push_z == false)
+      digitalWrite(led, HIGH);
+    else if (push_z == false)
+      digitalWrite(led, LOW);
+    push_z = true;
+  }
+  else
+    push_z = false;
+
+  if (nunchuk.cButton == 1)
+  {
+    if (!digitalRead(relais) && push_c == false)
+      digitalWrite(relais, HIGH);
+    else if (push_c == false)
+      digitalWrite(relais, LOW);
+    push_c = true;
+  }
+  else
+    push_c = false;
+
+  // Impression des valeurs du nunchuk
+  // Joystick XY
+  Serial.print(nunchuk.analogX, DEC);  // 0-255 X, 125 au neutre
   Serial.print(' ');
   Serial.print(nunchuk.analogY, DEC);
   Serial.print(' ');
-  
-  //Accelerometre XYZ
+  // Accelerometre XYZ
   Serial.print(nunchuk.accelX, DEC);
   Serial.print(' ');
   Serial.print(nunchuk.accelY, DEC);
   Serial.print(' ');
   Serial.print(nunchuk.accelZ, DEC);
   Serial.print(' ');
-  
-  //Deux boutons de la Nunchuck Wii
+  // Boutons c et z
   Serial.print(nunchuk.zButton, DEC);
   Serial.print(' ');
   Serial.println(nunchuk.cButton, DEC);
 
-  if (nunchuk.zButton == 1)
-  {
-    digitalWrite(led, HIGH);
-    digitalWrite(relais, HIGH);
-    //tone(buz, 50, 50);
-    //tone(buz, 0, 50);
-  }
-  else
-  {
-    digitalWrite(led, LOW);
-    digitalWrite(relais, LOW);
-  }
 }
